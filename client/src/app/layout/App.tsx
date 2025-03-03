e964a2bf54394c37fe5d1db5f11ee02736eb15f6
Umutuka2000 <71619569+Umutuka2000@users.noreply.github.com>
2025-03-03 16:45:23 +0000

    Initial UI Design

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
+import { Box, Container, createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import NavBar from "./NavBar";
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(true);
+  const palletType = darkMode ? "dark" : "light";
+
+  const theme = createTheme({
+    palette: {
+      mode: palletType,
+      background: {
+        default: palletType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Box sx={{ minHeight: "100vh", background: darkMode ? "radial-gradient(circle, #1e3aBa, #111B27)" : "radial-gradient(circle, #baecf9, #f0f9ff)", py: 6 }}>
+        <Container maxWidth="xl" sx={{ mt: 8 }}>
+          <Catalog products={products} />
+        </Container>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ client/src/app/layout/NavBar.tsx
@@ -0,0 +1,18 @@
+import { DarkMode, LightMode } from "@mui/icons-material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+
+type Props = {
+  darkMode: boolean;
+  setDarkMode: (darkMode: boolean) => void;
+};
+
+export default function NavBar({ darkMode, setDarkMode }: Props) {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6">RE-STORE</Typography>
+        <IconButton onClick={() => setDarkMode(!darkMode)}>{darkMode ? <DarkMode /> : <LightMode sx={{ color: "yellow" }} />}</IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
